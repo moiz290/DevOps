e4416a92cf6116c7865b3e507aad45be74c64ee4
Laviza Falak Naz <lavizaniazi2001@gmail.com>
2021-12-31 09:38:33 +0500

    database configurations

--- config/config.php
+++ config/config.php
@@ -1,9 +1,20 @@
 <?php
-    $servername = "db4free.net";
-    $username = "devops_4108845";
-    $password = "hello12eF";
-    $database = "devops_4108845";
+    $servername = "us-cdbr-east-05.cleardb.net";
+    $username = "bcc77e1841a73a";
+    $password = "dd32e024";
+    $database = "heroku_7fce67cb249adf3";
     $port = 3306;
+
+    /*
+     * fetch CONFIG VAR VALUES from heroku server and get the config values
+     * url obtained is:
+     * mysql://bcc77e1841a73a:dd32e024@us-cdbr-east-05.cleardb.net/heroku_7fce67cb249adf3?reconnect=true
+     * Everything:
+     * * after the @ symbol until the / is the DB_HOST, us-cdbr-east-05.cleardb.net 
+     * * Everything after / until ? is DB_DATABASE, heroku_7fce67cb249adf3
+     * * The string after the // until : is the DB_USERNAME, bcc77e1841a73a
+     * * The string between : and @ is the DB_PASSWORD, dd32e024
+     */
 
     // Create connection
     $conn = new mysqli($servername, $username, $password, $database, $port);
